44722ade7903d7a18ad29f2603d4d20bd27e058c
dajiehuangjin <dajiehuangjin@gmail.com>
2022-11-09 15:37:24 +0800

    消除告警

--- src/main/java/com/wq/springboot/entity/AccUser.java
+++ src/main/java/com/wq/springboot/entity/AccUser.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="acc_user")
-@SuppressWarnings("serial")
+//@SuppressWarnings("serial")
 public class AccUser implements Serializable {
 
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
